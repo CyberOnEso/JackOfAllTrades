368d3c717e311b3378ce70f86c9b151fa2b41a6b
CyberOnEso <benmillar742@gmail.com>
2021-02-22 11:52:01 +0000

    Version 0.5-alpha
    Created the language file

---
+++ lang/en.lua
@@ -0,0 +1,15 @@
+-------------------------------------------------------------------------------------------------
+-- Chat strings  --
+-------------------------------------------------------------------------------------------------
+ZO_CreateStringId("SI_JACK_OF_ALL_TRADES_METICULOUS_DISASSEMBLY_BENEFIT", "improved refining results")
+ZO_CreateStringId("SI_JACK_OF_ALL_TRADES_TREASURE_HUNTER_BENEFIT", "higher quality chest items")
+ZO_CreateStringId("SI_JACK_OF_ALL_TRADES_NOT_ENOUGH_POINTS_WARNING", "You don't have enough points allocated into the <<t:1>> star to get <<z:2>>.")
+
+-------------------------------------------------------------------------------------------------
+-- Menu strings  --
+-------------------------------------------------------------------------------------------------
+ZO_CreateStringId("SI_JACK_OF_ALL_TRADES_MENU_WARNING_DESCRIPTION", "Show warning if you do not have enough points allocated into the following CP Stars when they would be useful.")
+ZO_CreateStringId("SI_JACK_OF_ALL_TRADES_WARNING", "Warning")
+ZO_CreateStringId("SI_JACK_OF_ALL_TRADES_WARNING_TEXT_COLOUR", "<<C:1>> text colour")
+ZO_CreateStringId("SI_JACK_OF_ALL_TRADES_DEBUG", "Debug")
+ZO_CreateStringId("SI_JACK_OF_ALL_TRADES_ENABLE_MODE", "Enable <<z:1>> mode")
