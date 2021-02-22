9a1677ff175260ab224c2b798151996b2e351b79
CyberOnEso <benmillar742@gmail.com>
2021-02-22 11:52:29 +0000

    Version 0.5-alpha
    Created language files

---
+++ lang/de.lua
@@ -0,0 +1,15 @@
+-------------------------------------------------------------------------------------------------
+-- Chat strings  --
+-------------------------------------------------------------------------------------------------
+SafeAddString(SI_JACK_OF_ALL_TRADES_METICULOUS_DISASSEMBLY_BENEFIT, "Raffinierungsergebnisse verbessern");
+SafeAddString(SI_JACK_OF_ALL_TRADES_TREASURE_HUNTER_BENEFIT, "höherwertige Brustartikel");
+SafeAddString(SI_JACK_OF_ALL_TRADES_NOT_ENOUGH_POINTS_WARNING, "Sie haben nicht genügend punkte in der <<t:1>> stern zu bekommen <<z:2>>.");
+
+-------------------------------------------------------------------------------------------------
+-- Menu strings  --
+-------------------------------------------------------------------------------------------------
+SafeAddString(SI_JACK_OF_ALL_TRADES_MENU_WARNING_DESCRIPTION, "Zeigen Sie eine Warnung an, wenn Sie den folgenden CP-Sternen nicht genügend punkte zugewiesen haben, wenn sie nützlich wären.");
+SafeAddString(SI_JACK_OF_ALL_TRADES_WARNING, "Warning");
+SafeAddString(SI_JACK_OF_ALL_TRADES_WARNING_TEXT_COLOUR, "<<1>> text colour");
+SafeAddString(SI_JACK_OF_ALL_TRADES_DEBUG, "Debug");
+SafeAddString(SI_JACK_OF_ALL_TRADES_ENABLE_MODE, "Enable <<1>> mode");
---
+++ lang/fr.lua
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------------
+-- Chat strings  --
+-------------------------------------------------------------------------------------------------
+SafeAddString(SI_JACK_OF_ALL_TRADES_METICULOUS_DISASSEMBLY_BENEFIT, "amélioration des résultats de raffinage");
+SafeAddString(SI_JACK_OF_ALL_TRADES_TREASURE_HUNTER_BENEFIT, "articles de poitrine de meilleure qualité");
+SafeAddString(SI_JACK_OF_ALL_TRADES_NOT_ENOUGH_POINTS_WARNING, "Vous n'avez pas assez de points alloués dans le <<t:1>> étoile pour obtenir <<z:2>>.");
+
+-------------------------------------------------------------------------------------------------
+-- Menu strings  --
+-------------------------------------------------------------------------------------------------
+SafeAddString(SI_JACK_OF_ALL_TRADES_MENU_WARNING_DESCRIPTION, "Montrez un avertissement si vous ne disposez pas de suffisamment de points attribués aux étoiles CP suivantes alors qu'elles seraient utiles.");
+SafeAddString(SI_JACK_OF_ALL_TRADES_WARNING, "Warning");
+SafeAddString(SI_JACK_OF_ALL_TRADES_WARNING_TEXT_COLOUR, "<<1>> text colour");
+SafeAddString(SI_JACK_OF_ALL_TRADES_DEBUG, "Debug");
+SafeAddString(SI_JACK_OF_ALL_TRADES_ENABLE_MODE, "Enable <<1>> mode");
